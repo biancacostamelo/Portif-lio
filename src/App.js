1aa098020f53c98aba22ea365f19fe5e2f24cbe2
Bianca <bianca.costamelo1@gmail.com>
2025-02-02 23:31:15 -0300

    criando seção home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
+import React from "react";
+import Home from "./paginas/home";
+import Nav from "./components/nav";
 
 function App() {
   return (
-    <div className="App">
-     <h1>hello horld</h1>
-    </div>
+    <>
+      <Nav/>
+      <Home/>
+    </>
   );
 }
 
