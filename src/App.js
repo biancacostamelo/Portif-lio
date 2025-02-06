a07365cbc030f8a9d24fc0893a636390a191c7da
Bianca Costa <bianca.costamelo1@gmail.com>
2025-02-05 22:16:28 -0300

    criando seção de projetos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
-import Home from "./paginas/home";
+// import Home from "./paginas/home";
 import Nav from "./components/nav";
+import Projetos from "./paginas/projetos";
 
 function App() {
   return (
     <>
       <Nav/>
-      <Home/>
+      <Projetos/>
+      {/* <Home/> */}
     </>
   );
 }
---
+++ src/paginas/projetos.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/projetos.css'
+
+function Projetos() {
+  return (
+    <div id='projetos'>
+
+    </div>
+  )
+}
+
+export default Projetos
